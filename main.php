4b029dbb0de98bbfab9216f5a6d0c625344a24bc
MartineJacobsen <martinejacobsen90@gmail.com>
2018-05-24 08:54:25 +0200

    add one more value spot for csv-value

--- main.php
+++ main.php
@@ -13,18 +13,24 @@ class Main {
 
     function __construct() {
 
-        $this-> run_file();
+        $this->run_file();
 
     }
 
-    private function run_file() {
+    public function getStudents() {
+
+        return $this->students;
+
+    }
+
+    function run_file() {
 
         // Read CSV and make array
-        $students = array_map('str_getcsv', file('./csv/students.csv'));
-        $students = array_shift($students);
+        $students = array_map('str_getcsv', file('csv/students.csv'));
+        array_shift($students);
 
         foreach ($students as $student) {
-            $this->students[] = new Student ($student[0],$student[1],$student[2],$student[3],$student[4]);
+            $this->students[] = new Student($student[0],$student[1],$student[2],$student[3],$student[4],$student[5]);
         }
 
     }

85c250d3e811c8bab6e5a3e37227fd4aabc79a03
MartineJacobsen <martinejacobsen90@gmail.com>
2018-05-25 10:39:15 +0200

    change foreach loops

--- instructors.php
+++ instructors.php
@@ -14,10 +14,10 @@ include ('header.php');
 <div class="container">
     <div class="row">
 
-<?php foreach ($Courses as $Course) : ?>
-    <?php $courseinstructor = $Course->getInstructorName(); ?>
+<?php foreach ($Instructors as $Instructor) : ?>
+<?php $instructor_name = $Instructor->get_InstructorName(); ?>
         <div class="col-4">
-        <h2 class="m-t-2"><?php echo $courseinstructor; ?></h2>
+        <h2 class="m-t-2"><?php echo $Instructor->get_InstructorName(); ?></h2>
         <table class="table">
           <thead>
             <tr class="uppercase">
@@ -26,25 +26,23 @@ include ('header.php');
             </tr>
           </thead>
           <tbody>
-            <?php foreach ($Instructors as $Instructor) : ?>
+
+            <?php foreach ($Courses as $Course) : ?>
+                <?php
+                    $instructorname = $Course->getInstructorName();
+                ?>
                 <tr>
-                <?php $coursecode = $Course->getCourseCode(); ?>
-                <?php $instructorName = $Instructor->get_InstructorName(); ?>
-                <?php $courseCode = $Instructor->get_CourseCode(); ?>
-
-                <?php if ($instructorName == $courseinstructor and $coursecode == $coursecode) : ?>
-
+                    <?php if ($instructor_name == $instructorname) : ?>
                     <td class="text-center"><?php echo $Course->getCourseCode(); ?></td>
                     <td class="text-center"><?php echo $Course->getCourseName(); ?></td>
 
-                <?php endif; ?>
+                    <?php endif; ?>
                 </tr>
             <?php endforeach; ?>
     </tbody>
 </table>
 
 </div>
-
 <?php endforeach; ?>
 </div>
 </div>
